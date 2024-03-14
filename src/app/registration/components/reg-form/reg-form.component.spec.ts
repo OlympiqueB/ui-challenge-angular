46dbf58528acd454a89492e03ae8a22d9ff6f8c2
OlympiqueB <balzsr@gmail.com>
2024-03-14 12:38:12 +0100

    created reg-form component

---
+++ src/app/registration/components/reg-form/reg-form.component.spec.ts
@@ -0,0 +1,176 @@
+import { ComponentFixture, TestBed, fakeAsync, flush, waitForAsync } from '@angular/core/testing';
+
+import { RegFormComponent } from './reg-form.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegistrationService } from 'src/app/core/services/registration.service';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('RegFormComponent', () => {
+  let component: RegFormComponent;
+  let fixture: ComponentFixture<RegFormComponent>;
+  let registrationServiceSpy: jasmine.SpyObj<RegistrationService>;
+
+  beforeEach(waitForAsync(() => {
+    const spy = jasmine.createSpyObj('RegistrationService', ['createUser']);
+
+    TestBed.configureTestingModule({
+      declarations: [RegFormComponent],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+      providers: [{ provide: RegistrationService, useValue: spy }],
+    }).compileComponents();
+
+    registrationServiceSpy = TestBed.inject(
+      RegistrationService
+    ) as jasmine.SpyObj<RegistrationService>;
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should start with form invalid', () => {
+    expect(component.regForm.invalid).toBeTrue();
+  });
+
+  it('should become valid when all fields are correctly filled', () => {
+    const username = component.regForm.controls['username'];
+    const email = component.regForm.controls['email'];
+    const password = component.regForm.controls['password'];
+    const confirmPassword = component.regForm.controls['confirmPassword'];
+
+    username.setValue('testuser');
+    email.setValue('test@example.com');
+    password.setValue('password123');
+    confirmPassword.setValue('password123');
+
+    expect(component.regForm.valid).toBeTrue();
+  });
+
+  it('should become invalid if any required field is left empty', () => {
+    const username = component.regForm.controls['username'];
+    const email = component.regForm.controls['email'];
+    const password = component.regForm.controls['password'];
+    const confirmPassword = component.regForm.controls['confirmPassword'];
+
+    username.setValue('testuser');
+    email.setValue('');
+    password.setValue('password123');
+    confirmPassword.setValue('password123');
+
+    expect(component.regForm.valid).toBeFalse();
+  });
+
+  it('should become invalid if email format is incorrect', () => {
+    const username = component.regForm.controls['username'];
+    const email = component.regForm.controls['email'];
+    const password = component.regForm.controls['password'];
+    const confirmPassword = component.regForm.controls['confirmPassword'];
+
+    username.setValue('testuser');
+    email.setValue('invalidemail');
+    password.setValue('password123');
+    confirmPassword.setValue('password123');
+
+    expect(component.regForm.valid).toBeFalse();
+  });
+
+  it('should become invalid if password and confirm password fields do not match', () => {
+    const username = component.regForm.controls['username'];
+    const email = component.regForm.controls['email'];
+    const password = component.regForm.controls['password'];
+    const confirmPassword = component.regForm.controls['confirmPassword'];
+
+    username.setValue('testuser');
+    email.setValue('test@example.com');
+    password.setValue('password123');
+    confirmPassword.setValue('password');
+
+    expect(component.regForm.valid).toBeFalse();
+  });
+
+  it('should call submitForm method when submit button is clicked', () => {
+    spyOn(component, 'submitForm');
+
+    const username = component.regForm.controls['username'];
+    const email = component.regForm.controls['email'];
+    const password = component.regForm.controls['password'];
+    const confirmPassword = component.regForm.controls['confirmPassword'];
+
+    username.setValue('testuser');
+    email.setValue('test@example.com');
+    password.setValue('password123');
+    confirmPassword.setValue('password123');
+
+    fixture.detectChanges();
+    
+    const button = fixture.nativeElement.querySelector('button[type="click"]');
+    button.click();
+    expect(component.submitForm).toHaveBeenCalled();
+  });
+
+  it('should not do anything if the form is invalid during form submission', () => {
+    spyOn(component, 'submitForm');
+    component.regForm.controls['username'].setValue('testuser');
+    component.regForm.controls['email'].setValue('');
+    component.regForm.controls['password'].setValue('password123');
+    component.regForm.controls['confirmPassword'].setValue('password123');
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button[type="click"]');
+    button.click();
+
+    expect(component.submitForm).not.toHaveBeenCalled();
+  });
+
+  it('should call createUser method of RegistrationService with correct user data when form is valid during form submission', waitForAsync(() => {
+    const username = 'testuser';
+    const email = 'test@example.com';
+    const password = 'password123';
+
+    component.regForm.controls['username'].setValue(username);
+    component.regForm.controls['email'].setValue(email);
+    component.regForm.controls['password'].setValue(password);
+    component.regForm.controls['confirmPassword'].setValue(password);
+
+    fixture.detectChanges();
+
+    registrationServiceSpy.createUser.and.returnValue(of({}));
+
+    const button = fixture.nativeElement.querySelector('button[type="click"]');
+    button.click();
+
+    expect(registrationServiceSpy.createUser).toHaveBeenCalledWith({
+      username: username,
+      email: email,
+      password: password,
+    });
+  }));
+
+  it('should unsubscribe from pwInputSubscription on component destruction', () => {
+    spyOn(component.pwInputSubscription!, 'unsubscribe');
+    component.ngOnDestroy();
+    expect(component.pwInputSubscription!.unsubscribe).toHaveBeenCalled();
+  });
+
+  it('should return correct form controls for getters', () => {
+    expect(component.username).toEqual(component.regForm.controls['username']);
+    expect(component.email).toEqual(component.regForm.controls['email']);
+    expect(component.password).toEqual(component.regForm.controls['password']);
+    expect(component.confirmPassword).toEqual(
+      component.regForm.controls['confirmPassword']
+    );
+  });
+});
---
+++ src/app/registration/components/reg-form/reg-form.component.ts
@@ -0,0 +1,126 @@
+import { Component, OnDestroy } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { UserModel } from 'src/app/core/models/user.model';
+import { RegistrationService } from 'src/app/core/services/registration.service';
+
+@Component({
+  selector: 'app-reg-form',
+  templateUrl: './reg-form.component.html',
+  styleUrls: ['./reg-form.component.scss'],
+})
+export class RegFormComponent implements OnDestroy {
+  regForm!: FormGroup;
+  backendErrors?: any;
+  isFormSent: boolean = false;
+
+  usernameInputSubscription?: Subscription;
+  emailInputSubscription?: Subscription;
+  pwInputSubscription?: Subscription;
+
+  constructor(private regService: RegistrationService) {
+    this.regForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+
+    this.regForm.addControl(
+      'confirmPassword',
+      new FormControl('', [
+        Validators.required,
+        this.confirmPasswordValidator(this.regForm),
+      ])
+    );
+
+    this.usernameInputSubscription = this.username?.valueChanges.subscribe(
+      () => {
+        if (this.backendErrors && this.backendErrors.username) {
+          delete this.backendErrors.username;
+        }
+      }
+    );
+
+    this.emailInputSubscription = this.email?.valueChanges.subscribe(() => {
+      if (this.backendErrors && this.backendErrors.email) {
+        delete this.backendErrors.email;
+      }
+    });
+
+    this.pwInputSubscription = this.password?.valueChanges.subscribe(() => {
+      this.confirmPassword?.updateValueAndValidity();
+      if (!this.password?.value) {
+        this.confirmPassword?.setValue('');
+      }
+      if (this.backendErrors && this.backendErrors.password) {
+        delete this.backendErrors.password;
+      }
+    });
+  }
+
+  confirmPasswordValidator(form: FormGroup): ValidatorFn {
+    return (control: AbstractControl) => {
+      return form.get('password')?.value === control.value &&
+        control.value &&
+        form.get('password')?.valid
+        ? null
+        : { passwordNotConfirmed: true };
+    };
+  }
+
+  submitForm() {
+    if (this.regForm.invalid || this.isFormSent) return;
+
+    this.isFormSent = true;
+
+    if (this.regForm.valid) {
+      const newUser: UserModel = {
+        username: this.username?.value,
+        email: this.email?.value,
+        password: this.password?.value,
+      };
+
+      this.regService.createUser(newUser).subscribe({
+        next: (reply) => {
+          console.log(reply);
+          // navigate to thanks page or something
+        },
+        error: (err) => {
+          this.backendErrors = err.error.errors;
+          console.log(err);
+        },
+        complete: () => {
+          this.isFormSent = false;
+        },
+      });
+    }
+  }
+
+  get username() {
+    return this.regForm.get('username');
+  }
+
+  get email() {
+    return this.regForm.get('email');
+  }
+
+  get password() {
+    return this.regForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.regForm.get('confirmPassword');
+  }
+
+  ngOnDestroy(): void {
+    if (this.pwInputSubscription) {
+      this.pwInputSubscription.unsubscribe();
+    }
+  }
+}
