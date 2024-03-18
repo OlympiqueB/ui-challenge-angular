d46b71bed8697b3e790257824b55201d3bd6af64
OlympiqueB <balzsr@gmail.com>
2024-03-18 15:26:56 +0100

    fixed profile routing, logout

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { Subscription } from 'rxjs';
 import { UserDataModel } from '../../models/userData.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -12,15 +13,14 @@ export class NavbarComponent {
   userObject!: UserDataModel | null;
   userObjSubscription!: Subscription;
 
-  constructor(private auth: AuthService) {
-    this.userObjSubscription = this.auth.userObject.subscribe(
-      (user) => {
-        this.userObject = user;
-      }
-    )
+  constructor(private auth: AuthService, private router: Router) {
+    this.userObjSubscription = this.auth.userObject.subscribe((user) => {
+      this.userObject = user;
+    });
   }
 
   onSignOutClick() {
     this.auth.logout();
+    this.router.navigate(['/']);
   }
 }
